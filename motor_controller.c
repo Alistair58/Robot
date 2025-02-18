6b4576bce23668aaf6e9cbea2bb6b5f5c58902e6
Alistair58 <alistair@hanlonhome.co.uk>
2025-02-18 20:38:28 +0000

    Works

--- motor_controller.c
+++ motor_controller.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "btstack.h"
 #include "hardware/pwm.h"
 #include "motor_controller.h" //The gatt file compiled by cmake
+
 
 
 //TODO 
@@ -14,12 +16,11 @@
 #define CHAR_UUID_RX        "94f493c9_c579_41c2_87ac_e12c02455864"  // Receive Characteristic (phone -> pico)
 #define CHAR_UUID_TX        "94f493ca_c579_41c2_87ac_e12c02455864"  // Transmit Characteristic (pico -> phone)
 #define APP_AD_FLAGS 0x06 //The flag 0x06 indicates: LE General Discoverable Mode and BR/EDR ncot supported.
+#define MOTOR_CONTROL_PACKET 0xA1
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 static btstack_context_callback_registration_t context_registration;
 static hci_con_handle_t con_handle;
-static int  le_notification_enabled = 1;
-char counter_string[12] = "Hello World";
-int counter_string_len = 12;
+static int  connected = 0;
 const uint8_t adv_data[] = { //Advertising data
     // Flags general discoverable
     0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
@@ -29,15 +30,23 @@ const uint8_t adv_data[] = { //Advertising data
     0x03, BLUETOOTH_DATA_TYPE_INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, 0x10, 0xff,
 };
 const uint8_t adv_data_len = sizeof(adv_data);
+//Facing the direction of the robot
+const uint l_pwm = 21;
+const uint l_forwards = 20;
+const uint l_backwards = 19;
+const uint r_pwm = 16;
+const uint r_forwards = 17;
+const uint r_backwards = 18;
+uint l_slice_num;
+uint l_channel;
+uint r_slice_num;
+uint r_channel;
 static void ble_setup();
 static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
 static uint16_t att_read_callback(hci_con_handle_t con_handle, uint16_t att_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size);
 static int att_write_callback(hci_con_handle_t con_handle, uint16_t att_handle, uint16_t transaction_mode, uint16_t offset, uint8_t *buffer, uint16_t buffer_size);
-void motorTest();
-static void beat(void);
-static void heartbeat_handler(struct btstack_timer_source *ts);
-static void notify_callback(void *context);
-void ble_send_notification();
+void motor_control(uint8_t leftThrottle,uint8_t rightThrottle);
+void gpio_pins_init(void);
 
 int main()
 {
@@ -52,6 +61,8 @@ int main()
     // Turn on the LED
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     printf("\nLED on");    
+    gpio_pins_init();
+    
     ble_setup();
 
     //Bluetooth Host controller interface
@@ -63,14 +74,13 @@ int main()
     return 0;
     
 }
-static btstack_timer_source_t heartbeat;
-#define HEARTBEAT_PERIOD_MS 1000
+
 static void ble_setup(){
     l2cap_init();
     // setup SM (security manager): Display only
     sm_init();
     // setup ATT server
-    att_server_init(profile_data, att_read_callback, att_write_callback);    
+    att_server_init(profile_data,NULL, att_write_callback);//We don't have a readable characteristic
     printf("\natt server init complete");
     // setup advertisements
     uint16_t adv_int_min = 0x0030;
@@ -89,14 +99,6 @@ static void ble_setup(){
 
     // register for ATT event
     att_server_register_packet_handler(packet_handler);
-    sleep_ms(1000);
-    // set one-shot timer
-    heartbeat.process = &heartbeat_handler;
-    btstack_run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
-    btstack_run_loop_add_timer(&heartbeat);
-
-    // beat once
-    beat();
 }
 
 /* 
@@ -106,46 +108,6 @@ static void ble_setup(){
  *        - stop the counter after a disconnect
  *        - send a notification when the requested ATT_EVENT_CAN_SEND_NOW is received
  */
-static int counter = 0;
-
- static void beat(void){
-    printf("\nHello world");
-}
-static void heartbeat_handler(struct btstack_timer_source *ts){
-    printf("\nHeartbeat le_notification_enabled: %d",le_notification_enabled);
-    if (le_notification_enabled) {
-        beat();
-        ble_send_notification();
-        //DEVICE MUST HAVE NOTIFICATIONS ENABLED
-    }
-
-    btstack_run_loop_set_timer(ts, HEARTBEAT_PERIOD_MS);
-    btstack_run_loop_add_timer(ts);
-} 
-// called by the stack, to send a notification
-static void notify_callback(void *context) {
-    printf("\nnotify_callback calling att_server_notify");
-    int err = att_server_notify(
-        con_handle,
-        ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_VALUE_HANDLE,
-        (uint8_t*) counter_string, counter_string_len
-    );
-    if (err) {
-        printf("\nnotify_callback - error!");
-    }
-}
-
-// use ble_send_notification to send a notification
-void ble_send_notification() {
-    if (le_notification_enabled) {
-        printf("\nble_send_notification");
-        context_registration.callback = &notify_callback;
-        context_registration.context = NULL; //Always sending the same notification - no need to pass it values
-        // tell the stack to call the notify_callback if it is OK to send a notification:
-        uint8_t result = att_server_request_to_send_notification(&context_registration, con_handle);
-        printf("\nRequest result %d",result);
-    }
-}
 
 
 static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
@@ -161,37 +123,24 @@ static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *pack
     switch (hci_event) {
         case HCI_EVENT_DISCONNECTION_COMPLETE:
             printf("\nDisconnection complete");
-            le_notification_enabled = 0;
+            connected = 0;
             break;
+        case HCI_EVENT_LE_META:
+            switch(hci_event_le_meta_get_subevent_code(packet)){
+                case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
+                    printf("\nConnected");
+                    connected = 1;
+                    break;
+                default:
+                    break;
+            }
+            break;
+            
         default:
             break;
     }
 }
 
-/*
- * @section ATT Read
- *
- * @text The ATT Server handles all reads to constant data. For dynamic data like the custom characteristic, the registered
- * att_read_callback is called. To handle long characteristics and long reads, the att_read_callback is first called
- * with buffer == NULL, to request the total value length. Then it will be called again requesting a chunk of the value.
- * See Listing attRead.
- */
-
-/* LISTING_START(attRead): ATT Read */
-
-// ATT Client Read Callback for Dynamic Data
-// - if buffer == NULL, don't copy data, just return size of value
-// - if buffer != NULL, copy data and return number bytes copied
-// @param offset defines start of attribute value
-static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size){
-    printf("\nRead callback");
-    UNUSED(connection_handle);
-
-    if (att_handle == ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_VALUE_HANDLE){
-        return att_read_callback_handle_blob((const uint8_t *)counter_string, counter_string_len, offset, buffer, buffer_size);
-    }
-    return 0;
-}
 
 /*
  * @section ATT Write
@@ -209,13 +158,18 @@ static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_h
     printf("\nWrite callback");
     switch (att_handle){
         case ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_CLIENT_CONFIGURATION_HANDLE:
-            le_notification_enabled = little_endian_read_16(buffer, 0) == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
-            printf("\nle_notification_enabled %d",le_notification_enabled);
+            //le_notification_enabled = little_endian_read_16(buffer, 0) == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
             con_handle = connection_handle;
             break;
         case ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_VALUE_HANDLE:
-            printf("Write: transaction mode %u, offset %u, data (%u bytes): ", transaction_mode, offset, buffer_size);
+            //Characteristic packets are in the form: doubleCheck leftThrottle rightThrottle
+            //0xa1 is the doubleCheck first packet (MOTOR_CONTROL_PACKET)
+            printf("\nWrite: transaction mode %u, offset %u, data (%u bytes): ", transaction_mode, offset, buffer_size);
             printf_hexdump(buffer, buffer_size);
+            if(buffer[0]==MOTOR_CONTROL_PACKET){
+                motor_control(buffer[1],buffer[2]);
+            }
+            
             break;
         default:
             break;
@@ -223,57 +177,44 @@ static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_h
     return 0;
 }
 
-void motorTest(){
-    //Facing the direction of the robot
-    const uint l_pwm = 21;
-    const uint l_forwards = 20;
-    const uint l_backwards = 19;
-    const uint r_pwm = 16;
-    const uint r_forwards = 17;
-    const uint r_backwards = 18;
+void gpio_pins_init(){
+     //Enable PWM on GPIO 21
+     gpio_set_function(l_pwm, GPIO_FUNC_PWM);
+     l_slice_num = pwm_gpio_to_slice_num(l_pwm);
+     l_channel = pwm_gpio_to_channel(l_pwm);
+     gpio_set_function(r_pwm, GPIO_FUNC_PWM);
+     r_slice_num = pwm_gpio_to_slice_num(r_pwm);
+     r_channel = pwm_gpio_to_channel(r_pwm);
+ 
+     //Init direction pins
+     gpio_init(l_forwards);
+     gpio_init(l_backwards);
+     gpio_init(r_forwards);
+     gpio_init(r_backwards);
+     gpio_set_dir(l_forwards,GPIO_OUT);
+     gpio_set_dir(l_backwards,GPIO_OUT);
+     gpio_set_dir(r_forwards,GPIO_OUT);
+     gpio_set_dir(r_backwards,GPIO_OUT);
+}
+void motor_control(uint8_t leftThrottle,uint8_t rightThrottle){
+    //leftThrottle and rightThrottle are both 0<= <=100
 
-    //Enable PWM on GPIO 21
-    gpio_set_function(l_pwm, GPIO_FUNC_PWM);
-    uint l_slice_num = pwm_gpio_to_slice_num(l_pwm);
-    uint l_channel = pwm_gpio_to_channel(l_pwm);
-    gpio_set_function(r_pwm, GPIO_FUNC_PWM);
-    uint r_slice_num = pwm_gpio_to_slice_num(r_pwm);
-    uint r_channel = pwm_gpio_to_channel(r_pwm);
-
-    //Init direction pins
-    gpio_init(l_forwards);
-    gpio_init(l_backwards);
-    gpio_init(r_forwards);
-    gpio_init(r_backwards);
-    gpio_set_dir(l_forwards,GPIO_OUT);
-    gpio_set_dir(l_backwards,GPIO_OUT);
-    gpio_set_dir(r_forwards,GPIO_OUT);
-    gpio_set_dir(r_backwards,GPIO_OUT);
-
-    //Set direction pins to forwards
-    gpio_put(l_forwards,1);
-    gpio_put(l_backwards,0);
-    gpio_put(r_forwards,1);
-    gpio_put(r_backwards,0);
-    
-    
+    //Set direction pins
+    gpio_put(l_forwards,(leftThrottle<50)?0:1);
+    gpio_put(l_backwards,(leftThrottle<50)?1:0);
+    gpio_put(r_forwards,(rightThrottle<50)?0:1);
+    gpio_put(r_backwards,(rightThrottle<50)?1:0);
 
     // Set period of 1000 cycles (0 to 999 inclusive)
     pwm_set_wrap(l_slice_num, 999);
     pwm_set_wrap(r_slice_num, 999);
  
-    pwm_set_chan_level(l_slice_num, l_channel, 800); //80% duty cycle
-    pwm_set_chan_level(r_slice_num, r_channel, 800);
+    uint16_t l_duty = (uint16_t) round(((float)(abs(leftThrottle-50))/50) * 1000);
+    //Abs as we want the values furthest from 50 (0 and 100 to give the fastest outputs)
+    uint16_t r_duty = (uint16_t) round(((float)(abs(rightThrottle-50))/50) * 1000);
+    pwm_set_chan_level(l_slice_num, l_channel, l_duty); //set duty cycle
+    pwm_set_chan_level(r_slice_num, r_channel, r_duty);
     // Set the PWM running
     pwm_set_enabled(l_slice_num, true);
     pwm_set_enabled(r_slice_num, true);
-
-    sleep_ms(5000);
-
-    pwm_set_enabled(l_slice_num, false);
-    gpio_put(l_forwards,0);
-    gpio_put(l_backwards,0);
-    pwm_set_enabled(r_slice_num, false);
-    gpio_put(r_forwards,0);
-    gpio_put(r_backwards,0);
 }
