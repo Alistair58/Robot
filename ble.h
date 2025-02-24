08c41cf1b51b364bbbf651206f9a1c94951d2db8
Alistair58 <alistair@hanlonhome.co.uk>
2025-02-24 17:32:55 +0000

    Tidied up

---
+++ ble.h
@@ -0,0 +1,98 @@
+#define SERVICE_UUID        "94f493c8_c579_41c2_87ac_e12c02455864"  // UART Service
+#define CHAR_UUID_RX        "94f493c9_c579_41c2_87ac_e12c02455864"  // Write Characteristic (phone -> pico) 
+
+#define APP_AD_FLAGS 0x06 //The flag 0x06 indicates: LE General Discoverable Mode and BR/EDR ncot supported.
+#define MOTOR_CONTROL_PACKET 0xA1 //Inidcates the start of a motor control packet
+static btstack_packet_callback_registration_t hci_event_callback_registration;
+static btstack_context_callback_registration_t context_registration;
+static hci_con_handle_t con_handle;
+static int connected = 0;
+const uint8_t adv_data[] = { //Advertising data
+    // Flags general discoverable
+    0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
+    // Name
+    0x06, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'R','o','b','o','t'
+};
+const uint8_t adv_data_len = sizeof(adv_data);
+
+
+void ble_setup();
+static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+static int att_write_callback(hci_con_handle_t con_handle, uint16_t att_handle, uint16_t transaction_mode, uint16_t offset, uint8_t *buffer, uint16_t buffer_size);
+
+void ble_setup(){
+    l2cap_init();
+    // setup SM (security manager): Display only
+    sm_init();
+    // setup ATT server
+    att_server_init(profile_data,NULL, att_write_callback);//We don't have a readable characteristic
+    printf("\natt server init complete");
+    // setup advertisements
+    uint16_t adv_int_min = 0x0030;
+    uint16_t adv_int_max = 0x0030;
+    uint8_t adv_type = 0;
+    bd_addr_t null_addr;
+    memset(null_addr, 0, sizeof(bd_addr_t));
+    gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0, null_addr, 0x07, 0x00);
+    gap_advertisements_set_data(adv_data_len, (uint8_t*) adv_data);
+    gap_advertisements_enable(1);
+    // register for HCI events
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+    // register for ATT event
+    att_server_register_packet_handler(packet_handler);
+    //Bluetooth Host controller interface
+    hci_power_control(HCI_POWER_ON);
+}
+
+//HCI events such as connection and disconnection
+//These are the lower level packet interactions
+static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
+    printf("\nPacket handler; packet_type %d",packet_type);
+    UNUSED(channel);
+    UNUSED(size);
+    bd_addr_t addr;
+    bd_addr_type_t addr_type; 
+    
+    if (packet_type != HCI_EVENT_PACKET) return;
+    uint8_t hci_event = hci_event_packet_get_type(packet);
+    printf("\nhci_event: %d",hci_event);
+    switch (hci_event) {
+        case HCI_EVENT_DISCONNECTION_COMPLETE:
+            printf("\nDisconnection complete");
+            connected = 0;
+            break;
+        case HCI_EVENT_LE_META:
+            switch(hci_event_le_meta_get_subevent_code(packet)){
+                case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
+                    printf("\nConnected");
+                    connected = 1;
+                    break;
+                default:
+                    break;
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+//Higher level ATT (GATT) characteristic writes (phone sends data to pico)
+static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t transaction_mode, uint16_t offset, uint8_t *buffer, uint16_t buffer_size){
+    switch (att_handle){
+        case ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_CLIENT_CONFIGURATION_HANDLE:
+            con_handle = connection_handle;
+            break;
+        case ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_VALUE_HANDLE:
+            //Characteristic packets are in the form: doubleCheck leftThrottle rightThrottle
+            //0xa1 is the doubleCheck first packet (MOTOR_CONTROL_PACKET)
+            if(buffer[0]==MOTOR_CONTROL_PACKET){
+                motor_control(buffer[1],buffer[2]);
+            }
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
--- motor_controller.c
+++ motor_controller.c
@@ -7,29 +7,6 @@
 #include "motor_controller.h" //The gatt file compiled by cmake
 
 
-
-//TODO 
-//Try running Le counter example in a clean project
-
-
-#define SERVICE_UUID        "94f493c8_c579_41c2_87ac_e12c02455864"  // UART Service
-#define CHAR_UUID_RX        "94f493c9_c579_41c2_87ac_e12c02455864"  // Receive Characteristic (phone -> pico)
-#define CHAR_UUID_TX        "94f493ca_c579_41c2_87ac_e12c02455864"  // Transmit Characteristic (pico -> phone)
-#define APP_AD_FLAGS 0x06 //The flag 0x06 indicates: LE General Discoverable Mode and BR/EDR ncot supported.
-#define MOTOR_CONTROL_PACKET 0xA1
-static btstack_packet_callback_registration_t hci_event_callback_registration;
-static btstack_context_callback_registration_t context_registration;
-static hci_con_handle_t con_handle;
-static int  connected = 0;
-const uint8_t adv_data[] = { //Advertising data
-    // Flags general discoverable
-    0x02, BLUETOOTH_DATA_TYPE_FLAGS, APP_AD_FLAGS,
-    // Name
-    0x06, BLUETOOTH_DATA_TYPE_COMPLETE_LOCAL_NAME, 'R','o','b','o','t', 
-    // Incomplete List of 16-bit Service Class UUIDs -- FF10 - only valid for testing!
-    0x03, BLUETOOTH_DATA_TYPE_INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, 0x10, 0xff,
-};
-const uint8_t adv_data_len = sizeof(adv_data);
 //Facing the direction of the robot
 const uint l_pwm = 21;
 const uint l_forwards = 20;
@@ -37,147 +14,35 @@ const uint l_backwards = 19;
 const uint r_pwm = 16;
 const uint r_forwards = 17;
 const uint r_backwards = 18;
-uint l_slice_num;
+uint l_slice_num; //Initialised in gpio_pins_init
 uint l_channel;
 uint r_slice_num;
 uint r_channel;
-static void ble_setup();
-static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
-static uint16_t att_read_callback(hci_con_handle_t con_handle, uint16_t att_handle, uint16_t offset, uint8_t * buffer, uint16_t buffer_size);
-static int att_write_callback(hci_con_handle_t con_handle, uint16_t att_handle, uint16_t transaction_mode, uint16_t offset, uint8_t *buffer, uint16_t buffer_size);
-void motor_control(uint8_t leftThrottle,uint8_t rightThrottle);
-void gpio_pins_init(void);
 
-int main()
-{
-    
+static void gpio_pins_init(void);
+void motor_control(uint8_t leftThrottle,uint8_t rightThrottle);
+#include "ble.h" //Contains ble_setup and the function which calls motor_control
+
+
+int main(){
     stdio_init_all();
 
-    // Initialise the Wi-Fi, Bluetooth and light chip
+    // Initialise the Wi-Fi, Bluetooth and LED chip
     if (cyw43_arch_init()) {
         printf("CYW43 init failed\n");
-        return -1;
+        return 1;
     }
     // Turn on the LED
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-    printf("\nLED on");    
     gpio_pins_init();
-    
     ble_setup();
-
-    //Bluetooth Host controller interface
-    hci_power_control(HCI_POWER_ON);
-
     while(1) {
         tight_loop_contents(); //Keep the device on
     }
     return 0;
-    
 }
 
-static void ble_setup(){
-    l2cap_init();
-    // setup SM (security manager): Display only
-    sm_init();
-    // setup ATT server
-    att_server_init(profile_data,NULL, att_write_callback);//We don't have a readable characteristic
-    printf("\natt server init complete");
-    // setup advertisements
-    uint16_t adv_int_min = 0x0030;
-    uint16_t adv_int_max = 0x0030;
-    uint8_t adv_type = 0;
-    bd_addr_t null_addr;
-    memset(null_addr, 0, 6);
-    gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0, null_addr, 0x07, 0x00);
-    gap_advertisements_set_data(adv_data_len, (uint8_t*) adv_data);
-    gap_advertisements_enable(1);
-    printf("\nAdvertising");
-
-    // register for HCI events
-    hci_event_callback_registration.callback = &packet_handler;
-    hci_add_event_handler(&hci_event_callback_registration);
-
-    // register for ATT event
-    att_server_register_packet_handler(packet_handler);
-}
-
-/* 
- * @section Packet Handler
- *
- * @text The packet handler is used to:
- *        - stop the counter after a disconnect
- *        - send a notification when the requested ATT_EVENT_CAN_SEND_NOW is received
- */
-
-
-static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
-    printf("\nPacket handler; packet_type %d",packet_type);
-    UNUSED(channel);
-    UNUSED(size);
-    bd_addr_t addr;
-    bd_addr_type_t addr_type; 
-    
-    if (packet_type != HCI_EVENT_PACKET) return;
-    uint8_t hci_event = hci_event_packet_get_type(packet);
-    printf("\nhci_event: %d",hci_event);
-    switch (hci_event) {
-        case HCI_EVENT_DISCONNECTION_COMPLETE:
-            printf("\nDisconnection complete");
-            connected = 0;
-            break;
-        case HCI_EVENT_LE_META:
-            switch(hci_event_le_meta_get_subevent_code(packet)){
-                case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
-                    printf("\nConnected");
-                    connected = 1;
-                    break;
-                default:
-                    break;
-            }
-            break;
-            
-        default:
-            break;
-    }
-}
-
-
-/*
- * @section ATT Write
- *
- * @text The only valid ATT writes in this example are to the Client Characteristic Configuration, which configures notification
- * and indication and to the the Characteristic Value.
- * If the ATT handle matches the client configuration handle, the new configuration value is stored and used
- * in the heartbeat handler to decide if a new value should be sent.
- * If the ATT handle matches the characteristic value handle, we print the write as hexdump
- * See Listing attWrite.
- */
-
-/* LISTING_START(attWrite): ATT Write */
-static int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle, uint16_t transaction_mode, uint16_t offset, uint8_t *buffer, uint16_t buffer_size){
-    printf("\nWrite callback");
-    switch (att_handle){
-        case ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_CLIENT_CONFIGURATION_HANDLE:
-            //le_notification_enabled = little_endian_read_16(buffer, 0) == GATT_CLIENT_CHARACTERISTICS_CONFIGURATION_NOTIFICATION;
-            con_handle = connection_handle;
-            break;
-        case ATT_CHARACTERISTIC_94f493ca_c579_41c2_87ac_e12c02455864_01_VALUE_HANDLE:
-            //Characteristic packets are in the form: doubleCheck leftThrottle rightThrottle
-            //0xa1 is the doubleCheck first packet (MOTOR_CONTROL_PACKET)
-            printf("\nWrite: transaction mode %u, offset %u, data (%u bytes): ", transaction_mode, offset, buffer_size);
-            printf_hexdump(buffer, buffer_size);
-            if(buffer[0]==MOTOR_CONTROL_PACKET){
-                motor_control(buffer[1],buffer[2]);
-            }
-            
-            break;
-        default:
-            break;
-    }
-    return 0;
-}
-
-void gpio_pins_init(){
+static void gpio_pins_init(){
      //Enable PWM on GPIO 21
      gpio_set_function(l_pwm, GPIO_FUNC_PWM);
      l_slice_num = pwm_gpio_to_slice_num(l_pwm);
@@ -196,7 +61,8 @@ void gpio_pins_init(){
      gpio_set_dir(r_forwards,GPIO_OUT);
      gpio_set_dir(r_backwards,GPIO_OUT);
 }
-void motor_control(uint8_t leftThrottle,uint8_t rightThrottle){
+
+void motor_control(uint8_t leftThrottle,uint8_t rightThrottle){ //Called by att_write_callback in ble.h
     //leftThrottle and rightThrottle are both 0<= <=100
 
     //Set direction pins
@@ -210,11 +76,11 @@ void motor_control(uint8_t leftThrottle,uint8_t rightThrottle){
     pwm_set_wrap(r_slice_num, 999);
  
     uint16_t l_duty = (uint16_t) round(((float)(abs(leftThrottle-50))/50) * 1000);
-    //Abs as we want the values furthest from 50 (0 and 100 to give the fastest outputs)
+    //Abs as we want the values furthest from 50 (0 and 100) to give the fastest outputs
     uint16_t r_duty = (uint16_t) round(((float)(abs(rightThrottle-50))/50) * 1000);
     pwm_set_chan_level(l_slice_num, l_channel, l_duty); //set duty cycle
     pwm_set_chan_level(r_slice_num, r_channel, r_duty);
-    // Set the PWM running
-    pwm_set_enabled(l_slice_num, true);
+    
+    pwm_set_enabled(l_slice_num, true); // Set the PWM running
     pwm_set_enabled(r_slice_num, true);
 }
