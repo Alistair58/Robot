ed89a658296d9ea7f175a4254b13854a54207ebb
Alistair58 <alistair@hanlonhome.co.uk>
2025-04-07 20:35:57 +0100

    Stepper motor code written and tested. Ultrasound sensor code written but it doesn't receive an echo. Most likely my dodgy electronics.

--- robot_control.c
+++ robot_control.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "pico/multicore.h"
 #include "btstack.h"
 #define BTSTACK_INCLUDED 1
 #include "hardware/pwm.h"
@@ -9,7 +10,9 @@
 #include "controller_ble.h" //The gatt file compiled by cmake
 
 
+
 //Facing the direction of the robot
+//Driving motors
 #define DM_L_PWM 21
 #define DM_L_FORWARDS 20
 #define DM_L_BACKWARDS 19
@@ -20,12 +23,22 @@ uint l_slice_num; //Initialised in gpio_pins_init
 uint l_channel;
 uint r_slice_num;
 uint r_channel;
+//Hall effect sensor
 #define HE_L_POWER 15
 #define HE_R_POWER 22
 #define HE_L_ADC_GPIO 27 //GPIO pin numbers
 #define HE_R_ADC_GPIO 26
 #define HE_L_ADC 1 //ADC pin numbers
 #define HE_R_ADC 0
+//Stepper motor
+#define SM_IN1 0
+#define SM_IN2 1
+#define SM_IN3 2
+#define SM_IN4 3
+//Ultrasound
+#define US_TRIG 4
+#define US_ECHO 5
+
 #define min(a,b) (((a)>(b))?(b):(a))
 
 static float curr_l_speed = 0;
@@ -37,13 +50,19 @@ float effective_l_throttle = 50;
 float effective_r_throttle = 50;
 
 static void gpio_pins_init(void);
-static void hall_effect_init(void);
 static void motor_speed_manage_blocking(void);
 static void he_update_speed(int ADC_PIN,bool *high, uint32_t *last_high,int32_t curr_time);
 static void manage_pwm(float curr_speed,uint8_t user_throttle,float *effective_throttle,uint32_t *last_pwm_change,uint32_t curr_time,uint slice_num,uint channel);
+static void stepper_motor_blocking(float radians,bool clockwise);
+static float ultrasound_distance(); 
 void update_throttle(uint8_t left_throttle_new,uint8_t right_throttle_new);
 #include "ble.h" //Contains ble_setup and the function which calls update_throttle
 
+void core1_main(){
+    sleep_ms(2000);
+    printf("\nStarting stepper motor");
+    stepper_motor_blocking(M_PI,true);
+}
 
 int main(){
     stdio_init_all();
@@ -56,32 +75,61 @@ int main(){
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     gpio_pins_init();
     ble_setup();
-    hall_effect_init();
+    multicore_launch_core1(core1_main);
     motor_speed_manage_blocking();
     return 0;
 }
 
 static void gpio_pins_init(){
-     //Enable PWM
-     gpio_set_function(DM_L_PWM, GPIO_FUNC_PWM);
-     l_slice_num = pwm_gpio_to_slice_num(DM_L_PWM);
-     l_channel = pwm_gpio_to_channel(DM_L_PWM);
-     gpio_set_function(DM_R_PWM, GPIO_FUNC_PWM);
-     r_slice_num = pwm_gpio_to_slice_num(DM_R_PWM);
-     r_channel = pwm_gpio_to_channel(DM_R_PWM);
-     // Set period of 1000 cycles (0 to 999 inclusive)
-     pwm_set_wrap(l_slice_num, 999);
-     pwm_set_wrap(r_slice_num, 999);
- 
-     //Init direction pins
-     gpio_init(DM_L_FORWARDS);
-     gpio_init(DM_L_BACKWARDS);
-     gpio_init(DM_R_FORWARDS);
-     gpio_init(DM_R_BACKWARDS);
-     gpio_set_dir(DM_L_FORWARDS,GPIO_OUT);
-     gpio_set_dir(DM_L_BACKWARDS,GPIO_OUT);
-     gpio_set_dir(DM_R_FORWARDS,GPIO_OUT);
-     gpio_set_dir(DM_R_BACKWARDS,GPIO_OUT);
+    //Enable PWM
+    gpio_set_function(DM_L_PWM, GPIO_FUNC_PWM);
+    l_slice_num = pwm_gpio_to_slice_num(DM_L_PWM);
+    l_channel = pwm_gpio_to_channel(DM_L_PWM);
+    gpio_set_function(DM_R_PWM, GPIO_FUNC_PWM);
+    r_slice_num = pwm_gpio_to_slice_num(DM_R_PWM);
+    r_channel = pwm_gpio_to_channel(DM_R_PWM);
+    // Set period of 1000 cycles (0 to 999 inclusive)
+    pwm_set_wrap(l_slice_num, 999);
+    pwm_set_wrap(r_slice_num, 999);
+
+    //Init motor direction pins
+    gpio_init(DM_L_FORWARDS);
+    gpio_init(DM_L_BACKWARDS);
+    gpio_init(DM_R_FORWARDS);
+    gpio_init(DM_R_BACKWARDS);
+    gpio_set_dir(DM_L_FORWARDS,GPIO_OUT);
+    gpio_set_dir(DM_L_BACKWARDS,GPIO_OUT);
+    gpio_set_dir(DM_R_FORWARDS,GPIO_OUT);
+    gpio_set_dir(DM_R_BACKWARDS,GPIO_OUT);
+
+    //Init stepper motor pins
+    gpio_init(SM_IN1);
+    gpio_init(SM_IN2);
+    gpio_init(SM_IN3);
+    gpio_init(SM_IN4);
+    gpio_set_dir(SM_IN1,GPIO_OUT);
+    gpio_set_dir(SM_IN2,GPIO_OUT);
+    gpio_set_dir(SM_IN3,GPIO_OUT);
+    gpio_set_dir(SM_IN4,GPIO_OUT);
+
+    //Init ultrasound pins
+    gpio_init(US_TRIG);
+    gpio_init(US_ECHO);
+    gpio_set_dir(US_TRIG,GPIO_OUT);
+    gpio_set_dir(US_ECHO,GPIO_IN);
+    gpio_pull_down(US_ECHO);
+
+    //Init hall effect pins and ADC
+    gpio_init(HE_L_POWER);
+    gpio_init(HE_R_POWER);
+    gpio_set_dir(HE_L_POWER,GPIO_OUT);
+    gpio_set_dir(HE_R_POWER,GPIO_OUT);
+    gpio_put(HE_L_POWER,1);
+    gpio_put(HE_R_POWER,1);
+
+    adc_init();
+    adc_gpio_init(HE_L_ADC_GPIO);
+    adc_gpio_init(HE_R_ADC_GPIO);
 }
 
 void update_throttle(uint8_t left_throttle_new,uint8_t right_throttle_new){ //Called by att_write_callback in ble.h
@@ -108,28 +156,6 @@ void update_throttle(uint8_t left_throttle_new,uint8_t right_throttle_new){ //Ca
     pwm_set_enabled(r_slice_num, true);
 }
 
-static void hall_effect_init(void){
-    gpio_init(HE_L_POWER);
-    gpio_init(HE_R_POWER);
-    gpio_set_dir(HE_L_POWER,GPIO_OUT);
-    gpio_set_dir(HE_R_POWER,GPIO_OUT);
-    gpio_put(HE_L_POWER,1);
-    gpio_put(HE_R_POWER,1);
-
-    adc_init();
-    adc_gpio_init(HE_L_ADC_GPIO);
-    adc_gpio_init(HE_R_ADC_GPIO);
-    
-    //left sensor:
-    //1.62v is about normal
-    //>1.9v when right in front of a magnet
-    //all 5 magnets behave similarly
-
-    //right sensor:
-    //1.52v is about normal
-    //>1.75v when magnet
-}
-
 static void motor_speed_manage_blocking(void){
     //currently recording the time between 2 spokes
     //therefore, can't tell the direction
@@ -140,25 +166,25 @@ static void motor_speed_manage_blocking(void){
     uint32_t curr_time = time_us_32();
     uint32_t last_l_pwm_update = curr_time;
     uint32_t last_r_pwm_update = curr_time;
-    //uint32_t last_print_debug = time_us_32();
     while (1) {
         curr_time = time_us_32();
         he_update_speed(HE_L_ADC,&l_high,&last_l_high,curr_time);
         he_update_speed(HE_R_ADC,&r_high,&last_r_high,curr_time);
         manage_pwm(curr_l_speed,user_l_throttle,&effective_l_throttle,&last_l_pwm_update,curr_time,l_slice_num,l_channel);
         manage_pwm(curr_r_speed,user_r_throttle,&effective_r_throttle,&last_r_pwm_update,curr_time,r_slice_num,r_channel);
-        
-        // if(curr_time-last_print_debug>1000000){
-        //     last_print_debug = curr_time;
-        //     printf("Voltage_r: %f V, Time_since_high_r: %lus, curr_r_speed: %frads^-1\n", voltage_r, (curr_time-last_r_high)/1000000,curr_r_speed);
-        //     printf("Voltage_l: %f V, Time_since_high_l: %lus, curr_l_speed: %frads^-1\n", voltage_l, (curr_time-last_l_high)/1000000,curr_l_speed);
-        //     sleep_ms(10);
-        // }
     }
 }
 
 static void he_update_speed(int ADC_PIN,bool *high, uint32_t *last_high,int32_t curr_time){
     const int max_spoke_wait = 2000000;
+    //left sensor:
+    //1.62v is about normal
+    //>1.9v when right in front of a magnet
+    //all 5 magnets behave similarly
+
+    //right sensor:
+    //1.52v is about normal
+    //>1.75v when magnet
     const float high_voltage = 1.75; //gap so it is more stable
     const float low_voltage = 1.65;
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
@@ -211,4 +237,137 @@ static void manage_pwm(float curr_speed,uint8_t user_throttle,float *effective_t
         *last_pwm_change = curr_time;
   
     }
+}
+
+static void stepper_motor_blocking(float radians,bool clockwise){
+    if(radians>((float)M_PI+0.05f)){
+        printf("\nAngle is too large for stepper motor - it will break the wires");
+        return;
+    }
+    float rotation_freq = 0.25f; //in hertz
+    int phases = 8;
+    int phases_per_cycle = 8;
+    int gear_ratio = 64;
+    float radians_per_cycle = (2*M_PI)/(gear_ratio*phases_per_cycle);
+    uint64_t usDelay = (uint64_t) (((float)1/(phases*phases_per_cycle*rotation_freq*gear_ratio))*1000000);
+    float radians_travelled = 0;
+    if(!clockwise){
+        while (radians_travelled<radians) {
+            //Sequence taken from: https://wiki.seeedstudio.com/Gear_Stepper_Motor_Driver_Pack/
+            //A is IN2, B is IN1, C is IN4, D is IN3
+            gpio_put(SM_IN2,0);//A
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,1);
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,0);
+            gpio_put(SM_IN1,0);//AB
+            gpio_put(SM_IN4,1);
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,0);//B
+            gpio_put(SM_IN4,1);
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,0);
+            gpio_put(SM_IN4,0);//BC
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,0);//C
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,0);//CD
+            gpio_put(SM_IN3,0);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,1);//D
+            gpio_put(SM_IN3,0);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,0);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,1);//DA
+            gpio_put(SM_IN3,0);
+            sleep_us(usDelay);
+            radians_travelled+=radians_per_cycle;
+        }
+    }
+    else{
+        while (radians_travelled<radians) {
+            //Reversed sequence
+            gpio_put(SM_IN2,0);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,1);//DA
+            gpio_put(SM_IN3,0);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,1);//D
+            gpio_put(SM_IN3,0);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,0);//CD
+            gpio_put(SM_IN3,0);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,0);//C
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,0);
+            gpio_put(SM_IN4,0);//BC
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,1);
+            gpio_put(SM_IN1,0);//B
+            gpio_put(SM_IN4,1);
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,0);
+            gpio_put(SM_IN1,0);//AB
+            gpio_put(SM_IN4,1);
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            gpio_put(SM_IN2,0);//A
+            gpio_put(SM_IN1,1);
+            gpio_put(SM_IN4,1);
+            gpio_put(SM_IN3,1);
+            sleep_us(usDelay);
+            radians_travelled+=radians_per_cycle;
+        }
+    }
+    gpio_put(SM_IN2,0);
+    gpio_put(SM_IN1,0);
+    gpio_put(SM_IN4,0);
+    gpio_put(SM_IN3,0);   
+}
+
+static float ultrasound_distance(){ //Blocking - but not for too long
+    int speed_of_sound = 343; //ms^-1
+    uint32_t trig_time = time_us_32();
+    gpio_put(US_TRIG,1);
+    sleep_us(10);
+    gpio_put(US_TRIG,0);
+    uint32_t curr_time = time_us_32();
+    int echo_read = gpio_get(US_ECHO);
+    //TODO - the sensor does not work like this, it sets the echo high until the actual echo is received.
+    while(!echo_read){
+        echo_read = gpio_get(US_ECHO);
+        curr_time = time_us_32();
+        if(curr_time-trig_time>1000000){ //>100ms and there is a problem
+            return -1;
+        }
+    }
+    float time_diff_seconds = (float)(curr_time - trig_time)/1000000; //float can do 7 decimal digits, 1 mm away is 5.88*10^-6 seconds
+    return 2*speed_of_sound*time_diff_seconds; //Assuming a straight line
+
+
 }
