5f56f0e1c1e1045055aa254cfcb28cb65c8be0ca
Alistair58 <alistair@hanlonhome.co.uk>
2025-07-21 14:08:48 +0100

    - Fixed a few things. This is the program from the video sent to the eBay seller

--- imu.c
+++ imu.c
@@ -6,7 +6,8 @@ static int mpu9250_write(uint8_t *buffer,int len);
 static bool mpu9250_test();
 static bool imu_mag_test();
 static void imu_mag_bypass_scan();
-static void imu_gyro_read(uint16_t gyroData[3]);
+static void imu_mag_read(int16_t magData[3]);
+static void imu_gyro_read(int16_t gyroData[3]);
 static int mpu9250_write_byte(uint8_t reg,uint8_t byte);
 i2c_inst_t *i2c = i2c1;
 
@@ -81,7 +82,7 @@ static bool mpu9250_test(){
     uint8_t whoami_res = 0x00;
     mpu9250_read(WHOAMI_ADDR,&whoami_res,1);
 
-    printf("\nWhoami: %x",whoami_res);
+    printf("\nWhoami: 0x%x",whoami_res);
     const uint8_t MPU9250_WHOAMI = 0x71;
     const uint8_t MPU6500_WHOAMI = 0x70;
     //Mine seems to be 0x70
@@ -97,6 +98,72 @@ static bool mpu9250_test(){
 
 
 static bool imu_mag_test(){
+    //Tell it to access magnetometer
+    //Send it a DEVICE_ID request
+
+    //Slave 0 control
+    //The MPU-9250 communicates with its slave the magnetometer
+
+    //Disable I2C bypass mode
+    const uint8_t INT_PIN_CFG_ADDR = 0x37;
+    const uint8_t INT_PIN_CFG_VAL = 0x00;
+    mpu9250_write_byte(INT_PIN_CFG_ADDR,INT_PIN_CFG_VAL); 
+
+
+    const uint8_t USER_CTRL_ADDR = 0x6A;
+    //Enable the MPU-9250 being the master to the magnetometer
+    const uint8_t USER_CTRL_MASTER_ENABLE = 0x20;
+    mpu9250_write_byte(USER_CTRL_ADDR,USER_CTRL_MASTER_ENABLE);
+
+   
+    const uint8_t I2C_MST_CTRL_ADDR = 0x24;
+    //Set clock speed to 400kHz
+    const uint8_t I2C_MST_CTRL_CLK = 0x0D; 
+    mpu9250_write_byte(I2C_MST_CTRL_ADDR,I2C_MST_CTRL_CLK);
+
+    const uint8_t I2C_SLV0_ADDR_ADDR  = 0x25;
+    //Reading - MSB is 1
+    //From address 0x0C which is the magnetometer - LSB is 0x0C
+    const uint8_t I2C_SLV0_ADDR_VAL = 0x80|IMU_MAG_ADDR;
+    mpu9250_write_byte(I2C_SLV0_ADDR_ADDR,I2C_SLV0_ADDR_VAL);
+
+    const uint8_t I2C_SLV0_REG_ADDR = 0x26;
+    const uint8_t I2C_SLV0_REG_VAL = 0x00; //Device ID register
+    mpu9250_write_byte(I2C_SLV0_REG_ADDR,I2C_SLV0_REG_VAL);
+
+    const uint8_t I2C_SLV0_CTRL_ADDR = 0x27;
+    //Enable reading data from the slave - MSB is 1 (0x80)
+    //The registers start at odd numbers set bit 4 to be 1
+    //Reading 1 byte - LSB is 1
+    const uint8_t I2C_SLV0_CTRL_VAL = 0x80|0x01;
+    mpu9250_write_byte(I2C_SLV0_CTRL_ADDR,I2C_SLV0_CTRL_VAL);
+    
+    sleep_ms(100);
+    const uint8_t EXT_SENS_DATA_00_ADDR = 0x49;
+    uint8_t result = 0x00;
+    mpu9250_read(EXT_SENS_DATA_00_ADDR,&result,1);
+    printf("\nAK8963 (Magnetometer chip) Whoami: %x",result);
+    return result==0x48;
+}
+
+static void imu_mag_bypass_scan(){
+    printf("\nEnabling I2C bypass mode");
+    //Enable I2C bypass mode
+    const uint8_t INT_PIN_CFG_ADDR = 0x37;
+    const uint8_t INT_PIN_CFG_VAL = 0x02;
+    mpu9250_write_byte(INT_PIN_CFG_ADDR,INT_PIN_CFG_VAL); 
+    for (uint8_t addr=3;addr<0x78;addr++) {
+        // Just try to write 0 bytes — this is a common way to detect presence
+        uint8_t dummy = 0;
+        int result = i2c_write_blocking(i2c, addr, &dummy, 1, false);
+        if (result>0) {
+            printf("\nDevice found at 0x%02X. Bytes written: %d", addr,result);
+        }
+    }
+    printf("\nI2C bypass mode ending. Only the devices listed above were found.");
+}
+
+static void imu_mag_read(int16_t magData[3]){
     //Tell it to access magnetometer
     //Read from magnetometer
 
@@ -128,103 +195,48 @@ static bool imu_mag_test(){
 
     const uint8_t I2C_SLV0_REG_ADDR = 0x26;
     //Address of where to begin data transfer - Magnetometer values
-    const uint8_t I2C_SLV0_REG_VAL = 0x00; //Device ID register
+    const uint8_t I2C_SLV0_REG_VAL = 0x03; //Measurement data register
     mpu9250_write_byte(I2C_SLV0_REG_ADDR,I2C_SLV0_REG_VAL);
 
     const uint8_t I2C_SLV0_CTRL_ADDR = 0x27;
     //Enable reading data from the slave - MSB is 1 (0x80)
     //The registers start at odd numbers set bit 4 to be 1
-    //Reading 1 byte - LSB is 1
-    const uint8_t I2C_SLV0_CTRL_VAL = 0x80|0x01;
+    //Reading 6 bytes (x,y,z 2 each) - LSB is 6
+    const uint8_t I2C_SLV0_CTRL_VAL = 0x80|0b00010000|0x06;
     mpu9250_write_byte(I2C_SLV0_CTRL_ADDR,I2C_SLV0_CTRL_VAL);
     
+
+    //It will start storing data at EXT_SENS_DATA_00
     sleep_ms(100);
+    //Tell it that we want to access EXT_SENS_DATA_00
+    //We give it the register with a 1 at the MSB
+    //All registers are 7 bits long and a 1 MSB means read and a 0 MSB means write
+    uint8_t magDataRaw[6] = {0};
     const uint8_t EXT_SENS_DATA_00_ADDR = 0x49;
-    uint8_t result = 0x00;
-    mpu9250_read(EXT_SENS_DATA_00_ADDR,&result,1);
-    printf("\nAK8963 Whoami: %x",result);
-    return result==0x48;
-}
+    //The IMU then puts it in the FIFO and we read it
+    mpu9250_read(EXT_SENS_DATA_00_ADDR,magDataRaw,6);
 
-static void imu_mag_bypass_scan(){
-    //Enable I2C bypass mode
-    const uint8_t INT_PIN_CFG_ADDR = 0x37;
-    const uint8_t INT_PIN_CFG_VAL = 0x02;
-    mpu9250_write_byte(INT_PIN_CFG_ADDR,INT_PIN_CFG_VAL); 
-    for (uint8_t addr=3;addr<0x78;addr++) {
-        // Just try to write 0 bytes — this is a common way to detect presence
-        uint8_t dummy = 0;
-        int result = i2c_write_blocking(i2c, addr, &dummy, 1, false);
-        if (result>0) {
-            printf("\nDevice found at 0x%02X. Bytes written: %d", addr,result);
-        }
+    for (int i=0;i<3;i++) {
+        magData[i] = ((int16_t) magDataRaw[i*2] << 8 | magDataRaw[(i*2)+1]);
     }
+    printf("\nMagnetometer - X: %d Y: %d Z: %d",magData[0],magData[1],magData[2]);
+
 }
 
-// static void imu_mag_read(uint16_t magData[3]){
-//     //Tell it to access magnetometer
-//     //Read from magnetometer
-
-//     //Slave 0 control
-//     //The MPU-9250 communicates with its slave the magnetometer
-
-//     //I2C_MST_CTRL 0x24
-//     //Enable - MSB is 1
-//     //LSBs are clock settings - I've just gone with 0
-//     const uint8_t I2C_MST_CTRL_ADDR = 0x24;
-//     const uint8_t I2C_MST_CTRL_ENABLE = 0x80;
-
-//     //I2C_SLV0_ADDR 0x25
-//     //Reading - MSB is 1
-//     //From address 0x0C which is the magnetometer - LSB is 0x0C
-//     const uint8_t I2C_SLV0_ADDR_VAL = 0x80|0x0C;
-
-//     //I2C_SLV0_REG (0x26)
-//     //Address of where to begin data transfer - Magnetometer values
-//     const uint8_t I2C_SLV0_REG_VAL = 0x00; //Device ID register
-//     //I2C_SLV0_EN (0x27)
-//     //Enable reading data from the slave - MSB is 1 (0x80)
-//     //The registers start at odd numbers set bit 4 to be 1
-//     //Reading 6 bytes (2 bytes * x,y,z) - LSB is 6
-
-//     uint8_t buffer[] = {0x24,0x80,0x0C|0x80,IMU_MAG_VALUES_ADDR,0x80|0b10000|0x6};
-//     i2c_write_blocking(i2c,IMU_I2C_ADDR,buffer,5,false);
-
-//     //It will start storing data at EXT_SENS_DATA_00
-//     sleep_ms(10);
-//     //Tell it that we want to access EXT_SENS_DATA_00
-//     //We give it the register with a 1 at the MSB
-//     //All registers are 7 bits long and a 1 MSB means read and a 0 MSB means write
-//     uint8_t buffer2[] = {0x49|0x80};
-//     i2c_write_blocking(i2c,IMU_I2C_ADDR,buffer2,1,false);
-
-//     //The IMU then puts it in the FIFO and we read it
-//     sleep_ms(10);
-//     uint8_t magDataRaw[6] = {0};
-//     i2c_read_blocking(i2c,IMU_I2C_ADDR,magDataRaw,6,false);
-
-//     for (int i=0;i<3;i++) {
-//         magData[i] = (magDataRaw[i*2] << 8 | magDataRaw[(i*2)+1]);
-//     }
-//     printf("\nMagnetometer - X: %d Y: %d Z: %d",magData[0],magData[1],magData[2]);
-
-//     //Disable slave?
-// }
-
-static void imu_gyro_read(uint16_t gyroData[3]){
+static void imu_gyro_read(int16_t gyroData[3]){
     const uint8_t GYRO_XOUT_H_ADDR = 0x43;
     uint8_t gyroDataRaw[6] = {0};
     mpu9250_read(GYRO_XOUT_H_ADDR,gyroDataRaw,6);
     for (int i=0;i<3;i++) {
-        gyroData[i] = (gyroDataRaw[i*2] << 8 | gyroDataRaw[(i*2)+1]);
+        gyroData[i] = ((int16_t)gyroDataRaw[i*2] << 8 | gyroDataRaw[(i*2)+1]);
     }
     printf("\nGyroscope - X: %d Y: %d Z: %d",gyroData[0],gyroData[1],gyroData[2]);
 }
 
 void imu_i2c_init(void){
     //TODO
-    //remove
-    sleep_ms(2000);
+    //remove sleep
+    sleep_ms(20000);
     i2c_init(i2c,400*1000); //400kHz
     gpio_set_function(IMU_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(IMU_I2C_SCL_PIN, GPIO_FUNC_I2C);
@@ -233,18 +245,16 @@ void imu_i2c_init(void){
 
     bool connected = mpu9250_test();
     if(!connected) return;
-    
-
     imu_reset();
-
     imu_mag_test();
     imu_mag_bypass_scan();
-    
 
     while(1){
-        uint16_t gyroVals[3] = {0};
+        int16_t gyroVals[3] = {0};
         imu_gyro_read(gyroVals);
-        sleep_ms(1000);
+        int16_t magVals[3] = {0};
+        imu_mag_read(magVals);
+        sleep_ms(2000);
     }
 
     
